7cd7ed104de745e0e24d4add0fa8521b62abe488
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2025-02-09 00:12:21 -0300

    feat(neo-tree): add dependencies and update options

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,9 +1,26 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
     opts = {
         filesystem = {
             filtered_items = {
                 visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = {
+                    -- add extension names you want to explicitly exclude
+                    '.git',
+                    '.DS_Store',
+                    '.github',
+                    -- 'thumbs.db',
+                },
+                never_show = {},
             },
         },
         window = { position = "right" },
